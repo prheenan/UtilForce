9e09bf4578df70722c3c2e22c500d280ab2055de
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2018-03-02 17:55:04 -0700

    updated subrepos, removed unicdoe stuff

--- FEC/FEC_Util.py
+++ FEC/FEC_Util.py
@@ -1,6 +1,5 @@
 # force floating point division. Can still use integer with //
 from __future__ import division
-from __future__ import unicode_literals
 # This file is used for importing the common utilities classes.
 import numpy as np
 import matplotlib.pyplot as plt
